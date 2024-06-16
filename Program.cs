e95b58a4995f76249996e3031da6b5fb938a1fbf
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-16 23:42:08 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._5_String_manipulation__concatenation__substring_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string f_name = "Abdul";
+            string l_name = "Hannan";
+            Console.WriteLine("Full Name: " + f_name + " " + l_name);
+            string sentence = "The quick brown fox jumps over the lazy dog";
+            string sub = sentence.Substring(4,17);
+            Console.WriteLine($"SubString: {sub}");
+        }
+    }
+}
